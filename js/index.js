b3def1c47c07ccc01a0a10af3df1935a87d81fac
aditya rifki <adityarifkistemba@gmail.com>
2023-02-09 16:07:51 +0700

    revisi navbar

--- js/index.js
+++ js/index.js
@@ -1,24 +1,20 @@
 const myBody = document.querySelector("body");
-var topBtn = document.querySelector(".topBtn");
+const topBtn = $(".topBtn");
 let idleTime = 0;
 
 // fungsi yang akan dipanggil ketika tombol top button di klik
-function scrollToTop() {
-  myBody.scrollIntoView({ behavior: "smooth" });
-}
+// function scrollToTop() {
+//   myBody.scrollIntoView({ behavior: "smooth" });
+// }
 
 window.onscroll = function () {
   if ($(window).scrollTop() > 200) {
-    $(topBtn).css("display", "block");
+    topBtn.css("display", "block");
     idleTime = 0;
   } else {
-    $(topBtn).css("display", "none");
+    topBtn.css("display", "none");
   }
 };
-
-$(topBtn).click(function () {
-  scrollToTop();
-});
 
 // apabila terdapat gerakan pada mouse atau keyboard maka idleTime akan reset
 $(window).mousemove(() => {
@@ -32,6 +28,13 @@ $(window).keypress(() => {
 setInterval(() => {
   idleTime = idleTime + 1;
   if (idleTime > 4) {
-    $(topBtn).css("display", "none");
+    topBtn.css("display", "none");
   }
 }, 1000);
+
+topBtn.on("click", () => {
+  window.open(
+    "https://api.whatsapp.com/send/?phone=6281390153602&text&type=phone_number&app_absent=0",
+    "_blank" // <- This is what makes it open in a new window.
+  );
+});
